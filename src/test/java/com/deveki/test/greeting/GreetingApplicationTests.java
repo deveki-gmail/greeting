225baddab303ed86fa113c399d0a57d3202617c7
deveki-gmail <devekinandan@gmail.com>
2020-10-11 17:55:20 +0530

    some added test casses added.

--- src/test/java/com/deveki/test/greeting/GreetingApplicationTests.java
+++ src/test/java/com/deveki/test/greeting/GreetingApplicationTests.java
@@ -5,12 +5,18 @@ import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest
+//@RunWith(SpringRunner.class)
+//@SpringBootTest
 public class GreetingApplicationTests {
 
 	@Test
-	public void contextLoads() {
+	public void test1() {
+		System.out.println("Test 1");
+	}
+	
+	@Test
+	public void test2() {
+		System.out.println("Test 2");
 	}
 
 }
